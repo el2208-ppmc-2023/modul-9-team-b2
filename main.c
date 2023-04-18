e2f9c3d721b813a2bf91750300460b74418da7b8
gamalieladaran <13221018@std.stei.itb.ac.id>
2023-04-18 20:53:24 +0700

    Menambahkan headear pada file .c dan .h kemudian menambahkan file .csv

---
+++ main.c
@@ -0,0 +1,8 @@
+/*EL2208 Praktikum Pemecahan Masalah dengan C 2022/2023
+*Modul : 9 - Tugas Besar
+*Kelompok : B2
+*Hari dan Tanggal : 18 April 2023
+*Asisten (NIM) : Muhammad Daffa Rasyid (13220059)
+*Nama File : main.c
+*Deskripsi : ...
+*/
