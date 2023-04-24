3bfd98caa7b7ec66a24c18c63196cbbd6c05cdd8
vannyalviola <82189230+vannyalviola@users.noreply.github.com>
2023-04-24 13:39:09 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /*EL2208 Praktikum Pemecahan Masalah dengan C 2022/2023
 *Modul : 9 - Tugas Besar
 *Kelompok : B2
-*Hari dan Tanggal : 18 April 2023
+*Hari dan Tanggal : 24 April 2023
 *Asisten (NIM) : Muhammad Daffa Rasyid (13220059)
 *Nama File : main.c
 *Deskripsi : Program yang dapat membaca lokasi markas negara api kemudian membuat perbatasan efektif seluas-luasnya
