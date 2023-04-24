b5ec31073bd69be1dba034015c5a393ea41bc042
gamalieladaran <88667903+gamalieladaran@users.noreply.github.com>
2023-04-24 15:22:21 +0700

    Update main.h

--- main.h
+++ main.h
@@ -1,7 +1,7 @@
 /*EL2208 Praktikum Pemecahan Masalah dengan C 2022/2023
 *Modul : 9 - Tugas Besar
 *Kelompok : B2
-*Hari dan Tanggal : 18 April 2023
+*Hari dan Tanggal : 24 April 2023
 *Asisten (NIM) : Muhammad Daffa Rasyid (13220059)
 *Nama File : main.h
 *Deskripsi : file yang berisikan deklarasi tipe data bentukan 
